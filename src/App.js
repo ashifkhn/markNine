6be7c006248a75720f251dc34b29f9a3e7cbf742
Ashif Khan <54736284+ashifkhn@users.noreply.github.com>
2021-09-20 17:16:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+
+const songsDB = {
+  Prateek_Kuhad: [
+    { name: "Cold Mess", rating: "4.8" },
+    { name: "100 words", rating: "4.4" }
+  ],
+
+  Anuv_Jain: [
+    {
+      name: "Baarishen ",
+      rating: "4.9"
+    },
+    {
+      name: "Alag Asmaan",
+      rating: "4.7"
+    }
+  ],
+  Local_Train: [
+    {
+      name: "Choo Lo",
+      rating: "4.2"
+    },
+    {
+      name: "Dil Mere",
+      rating: "4.8"
+    }
+  ]
+};
+
+export default function App() {
+  const [selectedArtist, setArtist] = useState("Anuv_Jain");
+  function artistClickHandler(artist) {
+    setArtist(artist);
+  }
+  return (
+    <div className="App">
+      <h1 style={{ textAlign: "center" }}>MySongs</h1>
+      <p style={{ textAlign: "center", fontSize: "larger" }}>
+        Some of my favorite songs:
+      </p>
+      <div>
+        {Object.keys(songsDB).map((artist) => (
+          <button onClick={() => artistClickHandler(artist)}>
+            <ul>
+              <li>{artist}</li>
+            </ul>
+          </button>
+        ))}
+        <hr></hr>
+
+        <div>
+          <ul>
+            {songsDB[selectedArtist].map((artist) => (
+              <li key={artist.name}>
+                <div>
+                  {artist.name} {artist.rating}
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
